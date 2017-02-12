1c5bf363e73c2509620c8cda40c7a8da8610b527
utsavtulsyan <utsavtulsyan44@gmail.com>
2017-02-13 01:20:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 app.get('/one',function(req,res) {
   res.sendFile(path.join(_dirname,'ui','one.html'));
-}
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
